9c37f5e813ad2a2f67d011cf3ac2521edc8c5ba6
Jonathan Sutherland <jonathansutherland@Jonathans-Air.lan>
2024-05-24 09:48:06 -0400

    assignment

--- hello.py
+++ hello.py
@@ -1,10 +1,10 @@
 def hello_world():
     # Remove the # in the line below and delete the word pass
-    # print("Hello World!")
+    print("Hello World!")
 
-    pass
+
    
 
 # Do not modify the code below this line
 if __name__ == "__main__":
-    hello_world()
+    hello_world() 
